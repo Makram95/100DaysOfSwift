c1f5ea00296ce4197bf3c65024d7aa59e3cf6408
Makram95 <marc.kramer95@icloud.com>
2021-02-01 10:37:18 +0100

    Project7

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Marc on 01.02.21.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
