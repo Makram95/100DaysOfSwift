32a468dfa459c1e4ac61962ef23cc14cdef76546
Makram95 <marc.kramer95@icloud.com>
2021-02-08 09:21:06 +0100

    Project10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Marc on 08.02.21.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Marc on 08.02.21.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+    
+    
+}
